ee7f0b274e53cc7aaeffdf941fb415253b540857
Jeff Wainwright <yowainwright@gmail.com>
2018-05-22 14:57:29 -0700

    [init] adds initial actions

--- client/store.js
+++ client/store.js
@@ -1,10 +1,10 @@
-import { createStore, compose } from 'redux'
-import { syncHistoryWithStore } from 'react-router-redux'
-import { browserHistory } from 'react-router'
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { browserHistory } from 'react-router';
 
-import rootReducer from './reducers'
-import comments from './data/comments'
-import posts from './data/posts'
+import rootReducer from './reducers';
+import comments from './data/comments';
+import posts from './data/posts';
 
 const defaultState = {
   posts,
