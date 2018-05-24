c9529e392f08ab312e8fe9afbd1d8ef7b4c96ff3
Jeff Wainwright <yowainwright@gmail.com>
2018-05-24 14:20:50 -0700

    [update] adds reducers

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
-import css from './styles/style.styl';
-import Main from './components/Main'
-import Single from './components/Single'
-import PhotoGrid from './components/PhotoGrid'
 
-import { 
-    Router, 
-    Route, 
-    IndexRoute, 
-    browserHistory 
-} from 'react-router'
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import Components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 const router = (
     <Router history={browserHistory}>
-        <Route path='/' component={Main}>
+        <Route path="/" component={Main}>
             <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path='/view/:postId' component={Single}></Route>
+            <Route path="/view/:postId" component={Single}></Route>
         </Route>
     </Router>
 )
--- client/store.js
+++ client/store.js
@@ -1,16 +1,21 @@
-import { createStore, compose } from 'redux';
+import { createStore, compse } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
-import rootReducer from './reducers';
+// import the root reducer
+import rootReducer from './reducers/index';
+
 import comments from './data/comments';
 import posts from './data/posts';
 
+// create an object for the default data
 const defaultState = {
   posts,
   comments
 };
 
 const store = createStore(rootReducer, defaultState);
+
 export const history = syncHistoryWithStore(browserHistory, store);
+
 export default store;
