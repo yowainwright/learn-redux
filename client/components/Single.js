767b37d77cb9d0d7aa04f198c53764fd5ac2dfb5
Jeff Wainwright <yowainwright@gmail.com>
2018-05-24 21:43:22 -0700

    [update] connects react and redux = yup

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
     return (
       <div className="single-photo">
         I'm the single
