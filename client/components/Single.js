985c56e9354562a481fe702c551a72aa9f1b9bd8
Jeff Wainwright <yowainwright@gmail.com>
2018-05-28 23:47:43 -0700

    [update] adds comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Photo from './Photo';
-// import comments
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
     const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
-    this.props.posts[i]
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[]
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
+        <Comments />
       </div>
     )
   }
